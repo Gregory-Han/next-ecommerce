ac8a839e219eb3617bff11eb559f51fc3e20ee5b
Greg Han <greghan@naver.com>
2024-06-17 22:49:23 +0900

    chore: Update NavBar and SearchBar components with responsive design

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,31 @@
 "use client"
 
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+
+    const router = useRouter()
+    const onSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const name = formData.get('name')
+        console.log(name)
+
+        if (name) {
+            router.push(`/list?name=${name}`)
+        }
+    }
+    
+    return (
+        <form className="flex items-center justify-between gab-4 bg-gray-100 p-2 rounded-md w-full"
+            onSubmit={onSearch}>
+            <input type="text" name='name' placeholder="검색" className="w-full bg-transparent outline-none"/>
+            <button>
+                <Image src="/search.png" alt="search" width={18} height={18} />
+            </button>
+        </form>
+    )
 }
 
 export default SearchBar
